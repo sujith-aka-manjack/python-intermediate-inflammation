f77fea84f74d7c34f27204939d8619a1cfa3b629
sujith-aka-manjack <sujithjamesoo7@gmail.com>
2024-06-04 14:18:20 +0100

    Add parameterisation mean, min, max test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,44 +5,78 @@ import numpy.testing as npt
 import os
 import pytest
 
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
-def test_daily_max():
-    """Test that max function works for an array of positive integers."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0, 0], [0, 0, 0], [0, 0, 0] ], [0, 0, 0]),
+        ([ [4, 2, 5], [1, 6, 2], [4, 1, 9] ], [4, 6, 9]),
+        ([ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ], [4, -1, 9]),
+    ])
+def test_daily_max(test, expected):
+    """Test max function works for zeroes, positive integers, mix of positive/negative integers."""
     from inflammation.models import daily_max
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
-    test_input = np.array([[4, 2, 5],
-                           [1, 6, 2],
-                           [4, 1, 9]])
-    test_result = np.array([4, 6, 9])
-
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-
-def test_daily_min():
-    """Test that min function works for an array of positive and negative integers."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0, 0], [0, 0, 0], [0, 0, 0] ], [0, 0, 0]),
+        ([ [4, 2, 5], [1, 6, 2], [4, 1, 9] ], [1, 1, 2]),
+        ([ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ], [-4, -6, 2]),
+    ])
+def test_daily_min(test, expected):
+    """Test min function works for zeroes, positive integers, mix of positive/negative integers."""
     from inflammation.models import daily_min
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
-    test_input = np.array([[ 4, -2, 5],
-                           [ 1, -6, 2],
-                           [-4, -1, 9]])
-    test_result = np.array([-4, -6, 2])
+# def test_daily_max():
+#     """Test that max function works for an array of positive integers."""
+#     from inflammation.models import daily_max
 
-    npt.assert_array_equal(daily_min(test_input), test_result)
+#     test_input = np.array([[4, 2, 5],
+#                            [1, 6, 2],
+#                            [4, 1, 9]])
+#     test_result = np.array([4, 6, 9])
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
+#     npt.assert_array_equal(daily_max(test_input), test_result)
 
-def test_load_from_json(tmpdir):
-    from inflammation.models import load_json
-    example_path = os.path.join(tmpdir, 'example.json')
-    with open(example_path, 'w') as temp_json_file:
-        temp_json_file.write('[{"observations":[1, 2, 3]},{"observations":[4, 5, 6]}]')
-    result = load_json(example_path)
-    npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
 
-def test_daily_min_string():
-    """Test for TypeError when passing strings"""
-    from inflammation.models import daily_min
+# def test_daily_min():
+#     """Test that min function works for an array of positive and negative integers."""
+#     from inflammation.models import daily_min
 
-    with pytest.raises(TypeError):
-        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+#     test_input = np.array([[ 4, -2, 5],
+#                            [ 1, -6, 2],
+#                            [-4, -1, 9]])
+#     test_result = np.array([-4, -6, 2])
+
+#     npt.assert_array_equal(daily_min(test_input), test_result)
+
+#     # Need to use Numpy testing functions to compare arrays
+#     npt.assert_array_equal(daily_min(test_input), test_result)
+
+# def test_load_from_json(tmpdir):
+#     from inflammation.models import load_json
+#     example_path = os.path.join(tmpdir, 'example.json')
+#     with open(example_path, 'w') as temp_json_file:
+#         temp_json_file.write('[{"observations":[1, 2, 3]},{"observations":[4, 5, 6]}]')
+#     result = load_json(example_path)
+#     npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
+
+# def test_daily_min_string():
+#     """Test for TypeError when passing strings"""
+#     from inflammation.models import daily_min
+
+#     with pytest.raises(TypeError):
+#         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
